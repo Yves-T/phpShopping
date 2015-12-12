28a700a08fa67714f3a35c5a3fd84c210b30a880
Yves Talboom <yves.talboom@gmail.com>
2015-12-12 12:44:47 +0100

    reformat code

--- index.php
+++ index.php
@@ -14,50 +14,50 @@ function convertDecimalPoint($input)
 }
 
 include 'includes/html_start_inc.php';
-    include 'includes/db_inc.php';
+include 'includes/db_inc.php';
 
-    $stmt = $db->query("SELECT * FROM product");
-    $stmt->execute();
-    $results = $stmt->fetchAll();
+$stmt = $db->query("SELECT * FROM product");
+$stmt->execute();
+$results = $stmt->fetchAll();
+?>
+
+<?php
+$cookie_name = "userMessage";
+if (isset($_COOKIE[$cookie_name])):
     ?>
-
+    <div class="alert alert-success" role="alert"><p><?php print $_COOKIE[$cookie_name] ?></p></div>
     <?php
-    $cookie_name = "userMessage";
-    if (isset($_COOKIE[$cookie_name])):
-        ?>
-        <div class="alert alert-success" role="alert"><p><?php print $_COOKIE[$cookie_name] ?></p></div>
-        <?php
-        // remove cookie
-        unset($_COOKIE[$cookie_name]);
-        setcookie($cookie_name, null, -1, '/');
-    endif;
-    ?>
+    // remove cookie
+    unset($_COOKIE[$cookie_name]);
+    setcookie($cookie_name, null, -1, '/');
+endif;
+?>
 
-    <a href="addProduct.php" class="btn btn-success">Product toevoegen</a>
+<a href="addProduct.php" class="btn btn-success">Product toevoegen</a>
 
-    <?php include 'includes/start_ProductTable_inc.php' ?>
-    <?php foreach ($results as $row): ?>
+<?php include 'includes/start_ProductTable_inc.php' ?>
+<?php foreach ($results as $row): ?>
 
-        <tr>
-            <td><?php print $row['id']; ?></td>
-            <td><?php print $row['name']; ?></td>
-            <td><?php print $row['description']; ?></td>
-            <td><?php print $row['image']; ?></td>
-            <td><?php print $row['category']; ?></td>
-            <td><?php print convertDecimalPoint($row['price']); ?></td>
-            <td>
-                <a href="updateProduct.php?id=<?php print $row['id'] ?>" class="btn btn-success">Aanpassen</a>
-            </td>
-            <td class="delete" id="<?php print $row['id'] ?>">
-                <input id="<?php print $row['id'] ?>" type="submit" class="btn btn-danger" value="verwijderen">
-            </td>
-        </tr>
+    <tr>
+        <td><?php print $row['id']; ?></td>
+        <td><?php print $row['name']; ?></td>
+        <td><?php print $row['description']; ?></td>
+        <td><?php print $row['image']; ?></td>
+        <td><?php print $row['category']; ?></td>
+        <td><?php print convertDecimalPoint($row['price']); ?></td>
+        <td>
+            <a href="updateProduct.php?id=<?php print $row['id'] ?>" class="btn btn-success">Aanpassen</a>
+        </td>
+        <td class="delete" id="<?php print $row['id'] ?>">
+            <input id="<?php print $row['id'] ?>" type="submit" class="btn btn-danger" value="verwijderen">
+        </td>
+    </tr>
 
-    <?php endforeach; ?>
-    <script src="js/handleProductDelete.js"></script>
-    <?php include "includes/end_ProductTable_inc.php"; ?>
-    <?php
-    $db = NULL;
+<?php endforeach; ?>
+<script src="js/handleProductDelete.js"></script>
+<?php include "includes/end_ProductTable_inc.php"; ?>
+<?php
+$db = NULL;
 
 include 'includes/html_stop_inc.php'
 ?>
