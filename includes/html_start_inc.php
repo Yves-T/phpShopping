51d445a611186776178896daf7333fe37f1bfa20
Yves Talboom <yves.talboom@gmail.com>
2015-12-14 12:16:45 +0100

    move ckeditor dependency to lib folder

--- includes/html_start_inc.php
+++ includes/html_start_inc.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="werkingRTE/ckeditor/ckeditor.js"></script>
+    <script src="lib/ckeditor/ckeditor.js"></script>
     <style>
         #wrapper {
             padding: 10px;
