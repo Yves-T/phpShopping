b7497fb7efa4115e58cd1dd1e8267e604407c0e9
Yves Talboom <yves.talboom@gmail.com>
2015-12-14 12:21:30 +0100

    Add title to add product page

--- addProduct.php
+++ addProduct.php
@@ -37,6 +37,9 @@ if (!empty($_POST)) {
 $buttonText = "Toevoegen";
 
 include 'includes/html_start_inc.php';
+?>
+<h1>Product toevoegen</h1>
+<?php
 include 'includes/formvalidation/formErrors.php';
 include 'includes/product_form_inc.php';
 
