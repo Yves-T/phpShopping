1d99f8b22f4f78a5ca68f523abaa139bd62bc833
Yves Talboom <yves.talboom@gmail.com>
2016-01-13 16:25:39 +0100

    Move inline css styles to sass file

--- includes/html_start_inc.php
+++ includes/html_start_inc.php
@@ -7,15 +7,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="lib/ckeditor/ckeditor.js"></script>
-    <style>
-        #wrapper {
-            padding: 10px;
-        }
-
-        .row {
-            padding-left: 20px;
-        }
-    </style>
     <link rel="stylesheet" href="css/style.min.css">
 </head>
 <body>
