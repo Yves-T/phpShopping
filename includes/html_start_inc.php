68d2b5d0a0bf61e479bd9c98ae9555aeb6c07442
Yves Talboom <yves.talboom@gmail.com>
2016-01-13 14:30:31 +0100

    Add footer and nav bar to project

--- includes/html_start_inc.php
+++ includes/html_start_inc.php
@@ -16,6 +16,29 @@
             padding-left: 20px;
         }
     </style>
+    <link rel="stylesheet" href="css/style.min.css">
 </head>
 <body>
 <div id="wrapper">
+
+    <nav class="navbar navbar-default" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Shopping project</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Admin</a></li>
+            </ul>
+
+        </div><!-- /.navbar-collapse -->
+    </nav>
--- includes/html_stop_inc.php
+++ includes/html_stop_inc.php
@@ -1,3 +1,9 @@
 </div>
+<div id="footer">
+    <div class="container">
+        <p class="text-muted credit">Copyright &copy; <a href="http://www.cvodeverdieping.be/">CVO De Verdieping</a>
+            Schachtplein 1 3550 Heusden-zolder
+    </div>
+</div>
 </body>
 </html>
