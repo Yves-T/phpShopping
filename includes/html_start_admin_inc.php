7805d57e8cd17d65299008ca0ec9ba2e243f9f2f
Yves Talboom <yves.talboom@gmail.com>
2016-01-15 06:45:12 +0100

    Let brand in admin section navigate to index

--- includes/html_start_admin_inc.php
+++ includes/html_start_admin_inc.php
@@ -22,7 +22,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php">Shopping project</a>
+            <a class="navbar-brand" href="../index.php">Shopping project</a>
         </div>
 
         <?php
