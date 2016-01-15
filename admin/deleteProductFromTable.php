5684081055f2b8a64ecbe3a46655994ed0a78c7c
Yves Talboom <yves.talboom@gmail.com>
2016-01-15 06:58:28 +0100

    secure delete from product table

--- admin/deleteProductFromTable.php
+++ admin/deleteProductFromTable.php
@@ -2,6 +2,14 @@
 
 include '../includes/db_inc.php';
 
+if (!isset($_SESSION[USER])) {
+    $errorCookie = "errorMessage";
+    $errorCookieValue = "U hebt geen toegang tot de pagina die u hebt getracht te bezoeken!";
+    setcookie($errorCookie, $errorCookieValue, time() + (60), "/");
+    header('location: ../index.php');
+}
+
+
 $json = "";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
